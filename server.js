e9b42c7cf3d9c1394ae5bbabb319642987cea5bd
makachikava <chikavamakanaka@gmail.com>
2023-04-17 00:36:41 -0700

    added corsoptions

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors');
+var cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 require('dotenv').config()
 const movie = require('./models/moviesSchema')
 // middleware 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 const makaURI = process.env.MONGODB
 // ROUTES
 
