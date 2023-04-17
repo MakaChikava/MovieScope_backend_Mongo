d8f2750496b74e1d34a0f9afa901d83e75b7b3d1
makachikava <chikavamakanaka@gmail.com>
2023-04-17 00:42:07 -0700

    added corsoptions

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 var cors = require('cors');
 const corsOptions ={
     origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:false,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 require('dotenv').config()
